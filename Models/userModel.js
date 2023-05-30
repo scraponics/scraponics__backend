1f37e8d5888aedbda1e2006d516aa6c608ba636b
chao lobi <gogoiladitya@gmail.com>
2023-05-30 23:08:03 +0530

    making the schema for user

---
+++ Models/userModel.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    address: {
+        landmark: {
+            type: String,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        state: {
+            type: String,
+            required: true
+        },
+        postalCode: {
+            type: String,
+            required: true
+        }
+    },
+    profilePicture: String,
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    cart: [],
+    history: []
+
+}, { timestamps: true })
+
+const UserModel = mongoose.model("Users", UserSchema);
+export default UserModel;
