1eb292ff91b3a4bdeb4dff9afb3041b6135c62be
chao lobi <gogoiladitya@gmail.com>
2023-07-04 12:08:14 +0530

    trying to setup zustand

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -24,7 +24,7 @@ export const loginindUser = async (req, res) => {
         if (password == user[0].password) {
             res.status(200).json(user);
         } else {
-            res.status(400).json("User doesn't exist");
+            res.status(400).json(false);
         }
     } catch (error) {
         res.status(500).json({ message: error.message });
