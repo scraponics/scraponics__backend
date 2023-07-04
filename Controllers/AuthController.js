7b494fda251b4a401fe883edb4bca1eacd314b20
chao lobi <gogoiladitya@gmail.com>
2023-07-04 16:10:15 +0530

    duplicate users are now restricted

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -7,6 +7,10 @@ export const registerinduser = async (req, res) => {
     const newinduser = new IndividualModel({ name, email, password, landmark, district, state, postalCode, profilePicture, phoneNumber });
 
     try {
+        const oldUser = await IndividualModel.findOne({ email });
+        if (oldUser) {
+            res.status(400).json({ message: "user is already registered" });
+        }
         await newinduser.save();
         res.status(200).json(newinduser);
     } catch (error) {
@@ -24,7 +28,7 @@ export const loginindUser = async (req, res) => {
         if (password == user[0].password) {
             res.status(200).json(user);
         } else {
-            res.status(400).json(false);
+            res.status(400).json("Wrong password");
         }
     } catch (error) {
         res.status(500).json({ message: error.message });
